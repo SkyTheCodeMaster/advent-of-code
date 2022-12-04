71575102f258fce4c75dc768dbc6c4929316ce60
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2022-12-04 16:32:04 -0500

    Day 4

---
+++ 2022/4/4-2.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+import re
+
+if TYPE_CHECKING:
+  from typing import Tuple,List
+
+with open("4/input.txt") as f:
+  data = f.read()
+
+sections = re.compile("(\d+)\-(\d+)")
+
+def splitPairs(pair: str) -> Tuple[List[int],List[int]]:
+  one,two = pair.split(",")
+  reOne = sections.split(one)
+  reTwo = sections.split(two)
+  rangeOne = list(range(int(reOne[1]),int(reOne[2])+1))
+  rangeTwo = list(range(int(reTwo[1]),int(reTwo[2])+1))
+  return rangeOne,rangeTwo
+
+def contains(l1: list,l2: list) -> bool:
+  if (l1[0] <= l2[0] <= l1[-1]) or (l2[0] <= l1[0] <= l2[-1]):
+    return True
+  return False
+
+total = 0
+for pair in data.splitlines():
+  one,two = splitPairs(pair)
+  print(one,two,end=" | ")
+  contained = contains(one,two)
+  print(contained,end=" | ")
+  if contained:
+    total += 1
+  print(total)
+
+with open("4/output.txt","w") as f:
+  f.write(str(total))
